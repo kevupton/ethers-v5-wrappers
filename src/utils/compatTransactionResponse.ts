e1e2a67894481615739500aed607776ddef60de4
Kevin Upton <k_upton@live.com>
2024-03-30 16:48:56 +1100

    updated comment

--- src/utils/compatTransactionResponse.ts
+++ src/utils/compatTransactionResponse.ts
@@ -1,26 +1,6 @@
 import {Provider, TransactionResponse} from 'ethers';
 import type {TransactionResponse as V5TransactionResponse} from '@ethersproject/abstract-provider';
 import {formatTransactionResponse} from './format';
-//
-// {
-//   hash: string;
-//
-//   // Only if a transaction has been mined
-//   blockNumber?: number,
-//     blockHash?: string,
-//     timestamp?: number,
-//
-//     confirmations: number,
-//
-//   // Not optional (as it is in Transaction)
-//   from: string;
-//
-//   // The raw transaction
-//   raw?: string,
-//
-//     // This function waits until the transaction has been mined
-//     wait: (confirmations?: number) => Promise<TransactionReceipt>
-// }
 
 export const compatTransactionResponse = (
   response: V5TransactionResponse,
